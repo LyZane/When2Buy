4ada43bd0049b62a8e61dda81f2b41a833892600
Zane <lyzane.it@gmail.com>
2017-05-17 18:30:37 +0800

    新增属性“UnitPrice”和“ViewKey”。

--- Zane.When2Buy.Interface/Entity/GoodsEntity.cs
+++ Zane.When2Buy.Interface/Entity/GoodsEntity.cs
@@ -12,8 +12,16 @@ namespace Zane.When2Buy
         public string GoodsKey { get; set; }
         public Uri Uri { get; set; }
         public double Price { get; set; }
+        /// <summary>
+        /// 单位均价：元/克、升、毫升……
+        /// </summary>
+        public double? UnitPrice { get; set; }
         public string Name { get; set; }
         public string Specification { get; set; }
         public GoodsSource GoodsSource { get; set; }
+        /// <summary>
+        /// 视图唯一标识，当商品的名称或价格发生变化时，此标识会变更。
+        /// </summary>
+        public string ViewKey { get { return (Name + Price).GetHashCode().ToString(); } }
     }
 }
